94861c1f05e779a7950e49cfe8dc0ee46bd83d4a
Pedro <pee.dro_fs@hotmail.com>
2020-04-30 12:57:46 -0400

    Create routes for Poll show, create, vote and stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "poll/:id" => "polls#show"
+  post "poll" => "polls#create"
+
+  get "poll/:id/stats" => "polls#stats"
+  post "poll/:id/vote" => "polls#vote"
 end
