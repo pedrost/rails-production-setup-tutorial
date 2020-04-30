9a27d12ea83956dc8a8972aee728a23d5ab9254c
Pedro <pee.dro_fs@hotmail.com>
2020-04-30 12:56:27 -0400

    Create stats and show methods on Polls controller

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,31 +1,66 @@
-class ApplicationController < ActionController::Base
+class PollsController < ApplicationController
+  def show
+    poll = Poll.find_by(id: params[:id])
+    return render plain: "NOT FOUND", :status => :not_found if poll.blank?
 
-    def vote
-        option = self.options.find_by(id: vote_params[:option_id])
+    poll.viewed!
 
-        return render :status => :not_found if option.blank?
-        return render :status => :ok if option.vote!
+    return render :json => {
+      poll_id: poll.id,
+      poll_description: poll.poll_description,
+      options: poll.options.map { |active_record_option|
+        {
+          option_id: active_record_option.id,
+          option_description: active_record_option.option_description,
+        }
+      },
+    }, :status => :ok
+  end
+
+  def create
+    poll = Poll.new(poll_description: poll_params[:poll_description])
+
+    poll_params[:options].each do |option|
+      poll.options.new(option_description: option)
     end
 
-    def create
-        poll = Poll.new(poll_description: poll_params[:poll_description])
+    return render :json => { poll_id: poll.id }, :status => :created if poll.save
+    return render :json => poll.errors, :status => :unprocessable_entity
+  end
 
-        poll_params[:options].each do |option|
-            poll.options.new(options_description: option)
-        end
+  def vote
+    poll = Poll.find_by(id: params[:id])
+    option = poll.options.find_by(id: vote_params[:option_id]) if poll.present?
 
-        return render :json => { poll_id: poll.id }, :status => :created if poll.save
-        return render :json => poll.errors, :status => :unprocessable_entity
-    end
+    # if don't find option by id, try getting by index 0,1,2,3
+    option = poll.options[vote_params[:option_id]] if vote_params[:option_id].present? && option.blank?
 
-    private
+    return render plain: "NOT FOUND", status: :not_found if option.blank?
+    return render plain: "OK", status: :ok if option.vote!
+  end
 
-        def poll_params
-            params.permit(:poll_description, options: [])
-        end
+  def stats
+    poll = Poll.find_by(id: params[:id])
+    return render plain: "NOT FOUND", :status => :not_found if poll.blank?
 
-        def vote_params
-            params.permit(:option_id)
-        end
+    return render :json => {
+      views: poll.views,
+      options: poll.options.map { |active_record_option|
+        {
+          option_id: active_record_option.id,
+          qty: active_record_option.voted_times,
+        }
+      },
+    }, :status => :ok
+  end
 
+  private
+
+  def poll_params
+    params.permit(:poll_description, options: [])
+  end
+
+  def vote_params
+    params.permit(:option_id)
+  end
 end
